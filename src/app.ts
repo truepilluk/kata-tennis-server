136fb06e028fd372378d29e92d22e229c3ed12ce
Scott Arden-Jones <scott@truepill.com>
2022-11-02 04:45:26 +0000

    Add basic request logging

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,16 @@ import Application from 'koa';
 
 export const app = new Application();
 
+app.use(async (ctx, next) => {
+  await next();
+
+  console.log(
+    `${new Date().toISOString()} ${ctx.method} ${ctx.path} ${
+      ctx.status
+    } Request handled`
+  );
+});
+
 app.use((ctx) => {
   if (ctx.path === '/') {
     if (ctx.method === 'GET') {
