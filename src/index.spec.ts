bccd53b04a1d7861550cf84dc75fb6f8c2194d8e
Scott Arden-Jones <scott@truepill.com>
2022-11-02 04:03:18 +0000

    Add failing test for POST /

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,14 +1,20 @@
 import supertest from 'supertest';
 import Application from 'koa';
 
-it('should respond to GET / with a 200', async () => {
-  const app = new Application();
+const app = new Application();
 
-  app.use((ctx) => {
-    ctx.status = 200;
-  });
+app.use((ctx) => {
+  ctx.status = 200;
+});
 
+it('should respond to GET / with a 200 status', async () => {
   const res = await supertest(app.callback()).get('/');
 
   expect(res.status).toBe(200);
+});
+
+it('should respond to POST / with a 405 status', async () => {
+  const res = await supertest(app.callback()).post('/');
+
+  expect(res.status).toBe(405);
 });
