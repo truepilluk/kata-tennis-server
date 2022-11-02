a87eda96826a51d70073c568801464ae4af6c721
Scott Arden-Jones <scott@truepill.com>
2022-11-02 04:09:14 +0000

    Implement naive routing

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -4,7 +4,13 @@ import Application from 'koa';
 const app = new Application();
 
 app.use((ctx) => {
-  ctx.status = 200;
+  if (ctx.path === '/') {
+    if (ctx.method === 'GET') {
+      ctx.status = 200;
+    } else {
+      ctx.status = 405;
+    }
+  }
 });
 
 it('should respond to GET / with a 200 status', async () => {
@@ -17,4 +23,16 @@ it('should respond to POST / with a 405 status', async () => {
   const res = await supertest(app.callback()).post('/');
 
   expect(res.status).toBe(405);
+});
+
+it('should respond to GET /foo with a 404 status', async () => {
+  const res = await supertest(app.callback()).get('/foo');
+
+  expect(res.status).toBe(404);
+});
+
+it('should respond to POST /foo with a 404 status', async () => {
+  const res = await supertest(app.callback()).post('/foo');
+
+  expect(res.status).toBe(404);
 });
