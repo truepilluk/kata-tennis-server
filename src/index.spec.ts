51107bcb273729938b1cf0574c01e854854ebe84
Scott Arden-Jones <scott@truepill.com>
2022-11-02 03:58:23 +0000

    Add koa to implement HTTP server

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,7 +1,10 @@
 import supertest from 'supertest';
+import Application from 'koa';
 
 it('should respond to GET / with a 200', async () => {
-  const res = await supertest(app).get('/');
+  const app = new Application();
+
+  const res = await supertest(app.callback()).get('/');
 
   expect(res.status).toBe(200);
 });
