66514a1853e8d10ed8135a2b3335c49f52292f71
Scott Arden-Jones <scott@truepill.com>
2022-11-02 03:59:34 +0000

    Make GET / test pass

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -4,6 +4,10 @@ import Application from 'koa';
 it('should respond to GET / with a 200', async () => {
   const app = new Application();
 
+  app.use((ctx) => {
+    ctx.status = 200;
+  });
+
   const res = await supertest(app.callback()).get('/');
 
   expect(res.status).toBe(200);
